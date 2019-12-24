460523a36b76e5b1468723b1da99992e1a5f301d
yk1062008412 <yk1062008412@gmail.com>
2019-12-24 23:31:02 +0800

    购物车

--- src/components/shoppCar/shopCar.tsx
+++ src/components/shoppCar/shopCar.tsx
@@ -1,6 +1,6 @@
 import Taro, { Component } from '@tarojs/taro'
-import { View } from '@tarojs/components'
-import { AtFloatLayout, AtButton } from 'taro-ui'
+import { View, Image, Text } from '@tarojs/components'
+import { AtFloatLayout, AtButton, AtInputNumber } from 'taro-ui'
 import './shopCar.scss'
 
 export default class ShopCar extends Component <any, any> {
@@ -8,6 +8,7 @@ export default class ShopCar extends Component <any, any> {
   constructor(props) {
     super(props)
     this.state = {
+      count: 1
     }
   }
 
@@ -18,8 +19,31 @@ export default class ShopCar extends Component <any, any> {
     this.props.handleToggleCar()
   }
 
+  handleChange (e) {
+    this.setState({
+      count: e
+    })
+  }
+
+  handleClear () {
+    Taro.showModal({
+      title: '清空购物车',
+      content: '您确定要清空购物车吗？',
+    })
+    .then(res => {
+      if(res.confirm){
+        Taro.showToast({
+          title: '购物车已清空',
+          icon: 'none',
+          duration: 2000
+        })
+      }
+    })
+  }
+
   render() {
     const { carOpen } = this.props
+    const { count } = this.state
     return (
       <View className='shop-car-container'>
         {/* <AtButton onClick={this.handleToggleCar.bind(this)}>弹窗</AtButton> */}
@@ -29,8 +53,43 @@ export default class ShopCar extends Component <any, any> {
           onClose={this.handleToggleCar.bind(this)}
           className='container-layout'
         >
-          <View>
-          这是内容区 随你怎么写这是内容区 随你怎么写这是内容区 随你怎么写这是内容区.随你怎么写这是内容区 随你怎么写这是内容区 随你怎么写
+          <View className='car-container'>
+            <View className='car-header'>
+              <AtButton
+                circle
+                size='small'
+                className='car-header-button'
+                onClick={this.handleClear.bind(this)}>
+                清空
+              </AtButton>
+            </View>
+            <View className='car-body'>
+              <View className='car-item'>
+                <Image
+                  className='goods-image'
+                  src='https://dss0.bdstatic.com/6Ox1bjeh1BF3odCf/it/u=3533778697,2586993014&fm=74&app=80&f=PNG&size=f121,121?sec=1880279984&t=1dbed90be2871a78074bf731b6872ed0'
+                />
+                <View className='goods-content'>
+                  <View className='goods-info'>
+                    <View>商品名称</View>
+                    <View className='price-info'>
+                      <Text className='now-price'>¥10.00</Text>
+                      <Text className='origin-price'>¥15.00</Text>
+                    </View>
+                  </View>
+                  <View className='goods-number'>
+                    <AtInputNumber
+                      type='number'
+                      min={0}
+                      max={99}
+                      step={1}
+                      value={count}
+                      onChange={this.handleChange.bind(this)}
+                    />
+                  </View>
+                </View>
+              </View>
+            </View>
           </View>
         </AtFloatLayout>
       </View>
